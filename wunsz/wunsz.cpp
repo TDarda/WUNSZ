2c211d9a0f25eb68dfa4fae9c716f9f185c3687a
tomek <tomaszdarda@gmail.com>
2021-09-07 09:12:13 +0200

    collision with the border

--- wunsz/wunsz.cpp
+++ wunsz/wunsz.cpp
@@ -11,7 +11,7 @@
 using namespace std;
 
 char head = '@';
-int x = 1, y = 1;
+int x = 0, y = 1;
 char chooz;
 
 void direction(int x, int y) 
@@ -20,13 +20,16 @@ void direction(int x, int y)
     
     int szer = 40;
     int i = 0;
-    for (; i < y; i++) { cout <<setw(szer)<<"|" << endl; }
-    cout << setw(x) << head<<setw(szer-x)<<'|';
+    for (; i < y; i++) { cout <<setw(szer)<<"<" << endl; }
+    if (x == 39)cout << setw(39)<<head << '<';
+    else if (x == 0)cout << setw(39) << head << '<';
+    else
+    cout << setw(x) << head<<setw(szer-x)<<'<';
     i +=1;
     cout << endl;
-    for (; i < 20; i++) { cout << setw(szer) << "|" << endl; }
-    cout.fill('_');
-    cout << setw(39) << '_'<<right<<"|" << endl;
+    for (; i < 20; i++) { cout << setw(szer) << "<" << endl; }
+    cout.fill('^');
+    cout << setw(39) << '^';
     cout.fill(' ');
 }
 
@@ -43,6 +46,7 @@ int main()
                 case KEY_UP:
                     while (!_kbhit())
                     {
+                        if (y == -1)y = 19;
                         Sleep(330);
                         direction(x, y--);
                     };
@@ -50,6 +54,7 @@ int main()
                 case KEY_DOWN:
                     while (!_kbhit())
                     {
+                        if (y == 20)y = 0;
                         Sleep(330);
                         direction(x, y++);
                     };
@@ -57,6 +62,7 @@ int main()
                 case KEY_LEFT:
                     while (!_kbhit())
                     {
+                        if (x == 0)x = 39;
                         Sleep(330);
                         direction(x--, y);
                     };
@@ -64,6 +70,7 @@ int main()
                 case KEY_RIGHT:
                     while (!_kbhit())
                     {
+                        if (x == 39)x = 0;
                         Sleep(330);
                         direction(x++, y);
                     };
