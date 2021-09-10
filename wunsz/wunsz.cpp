4ae866a516bb0fdf68c2dba6ecdc90a3392156dd
tomek <tomaszdarda@gmail.com>
2021-09-10 22:07:36 +0200

    different concept

--- wunsz/wunsz.cpp
+++ wunsz/wunsz.cpp
@@ -1,87 +1,128 @@
-﻿#include <iostream>
-#include <iomanip>
-#include <conio.h>
-#include <windows.h>
+﻿#include <iomanip>
+#include<windows.h>
+#include<iostream>
+#include<conio.h>
+#include<deque>
 
-#define KEY_UP 72
-#define KEY_DOWN 80
-#define KEY_LEFT 75 
-#define KEY_RIGHT 77
+#define KEY_UP 72       //strzałka w górę
+#define KEY_DOWN 80     //strzałka w dół
+#define KEY_LEFT 75     //strzałka w lewo
+#define KEY_RIGHT 77    //strzałka w prawo
+#define HEIGHT 15    //wysokość poziomu
+#define WIDTH 30        //szerokość poziomu
 
 using namespace std;
 
-char head = '@';
-int x = 0, y = 1;
-char chooz;
+char Level[HEIGHT][WIDTH];
 
-void direction(int x, int y) 
+deque<pair<int, int>>snake = {
+        {7,4}, {7,5}, {7,6},{7,7}
+};
+
+void build_a_level() //budowa poziomu
+{
+    for (int i = 0; i < HEIGHT; i++)
+    {
+
+        for (int j = 0; j < WIDTH; j++)
+        {
+            Level[0][j] = '#';
+            Level[i][0] = '#';
+            Level[HEIGHT - 1][j] = '#';
+            Level[i][WIDTH - 1] = '#';
+        }
+    }
+}
+
+void delete_a_level() //czyszczecnie poziomu
+{
+    for (int i = 0; i < HEIGHT; i++)
+    {
+
+        for (int j = 0; j < WIDTH; j++)
+        {
+            Level[i][j] = ' ';
+
+        }
+    }
+}
+
+void create_a_snake()  //stworzenie węża na planszy
+{
+    for (deque<pair<int, int>>::iterator iter = snake.begin(); iter != snake.end(); iter++)
+    {
+        Level[iter->first][iter->second] = 'o';
+
+    }
+
+
+}
+
+void show_a_level()   //wyświetlenie poziomu
 {
     system("cls");
-    
-    int szer = 40;
-    int i = 0;
-    for (; i < y; i++) { cout <<setw(szer)<<"<" << endl; }
-    if (x == 39)cout << setw(39)<<head << '<';
-    else if (x == 0)cout << setw(39) << head << '<';
-    else
-    cout << setw(x) << head<<setw(szer-x)<<'<';
-    i +=1;
-    cout << endl;
-    for (; i < 20; i++) { cout << setw(szer) << "<" << endl; }
-    cout.fill('^');
-    cout << setw(39) << '^';
-    cout.fill(' ');
+    for (int i = 0; i < HEIGHT; i++)
+    {
+        cout << endl;
+
+        for (int j = 0; j < WIDTH; j++)
+        {
+            cout << Level[i][j];
+
+        }
+    }
+}
+
+void snake_manipulation(int& up_down, int& left_right)  //dodanie nowej głowy i usunięcię ogona
+{
+    snake.push_front(pair<int, int>(up_down, left_right));
+    snake.pop_back();
+
+}
+
+
+void game()  //właściwa gra
+{
+    bool game_on = true;
+    char choose, last_choose = KEY_RIGHT;
+    int snakeY = snake.front().first;
+    int snakeX = snake.front().second;
+
+    while (game_on)
+    {
+        choose = _getch();
+        switch (choose)
+        {
+        case KEY_UP:
+            while (!_kbhit())
+            {
+                Sleep(330);  delete_a_level(); snake_manipulation(--snakeY, snakeX); build_a_level(); create_a_snake(); show_a_level();
+            }
+            break;
+        case KEY_DOWN:
+            while (!_kbhit())
+            {
+                Sleep(330);  delete_a_level(); snake_manipulation(++snakeY, snakeX); build_a_level(); create_a_snake(); show_a_level();
+            }
+            break;
+        case KEY_RIGHT:
+            while (!_kbhit())
+            {
+                Sleep(330);  delete_a_level(); snake_manipulation(snakeY, ++snakeX); build_a_level(); create_a_snake();  show_a_level();
+            }
+            break;
+        case KEY_LEFT:
+            while (!_kbhit())
+            {
+                Sleep(330);  delete_a_level(); snake_manipulation(snakeY, --snakeX); build_a_level(); create_a_snake(); show_a_level();
+            }
+            break;
+        }
+
+    }
 }
 
 int main()
-{     
-     char wybor;
-         
-   
-        while (true)
-        {
-            wybor = _getch();
-            switch(wybor)
-            {
-                case KEY_UP:
-                    while (!_kbhit())
-                    {
-                        if (y == -1)y = 19;
-                        Sleep(330);
-                        direction(x, y--);
-                    };
-                break;
-                case KEY_DOWN:
-                    while (!_kbhit())
-                    {
-                        if (y == 20)y = 0;
-                        Sleep(330);
-                        direction(x, y++);
-                    };
-                break;
-                case KEY_LEFT:
-                    while (!_kbhit())
-                    {
-                        if (x == 0)x = 39;
-                        Sleep(330);
-                        direction(x--, y);
-                    };
-                break;
-                case KEY_RIGHT:
-                    while (!_kbhit())
-                    {
-                        if (x == 39)x = 0;
-                        Sleep(330);
-                        direction(x++, y);
-                    };
-                break;
-        
-            }
-        
-        }
-
-
-
- 
+{
+    game();
 }
-
